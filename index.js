6bcbd24625238db3f5cad3696983364c09f7ac80
CaptMoroni <dustiumus@gmail.com>
2025-05-26 23:11:08 -0600

    added style, foundation for about me section

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function toggleMe() {
+    const section = document.getElementById("aboutMe");
+    if (section.style.display === "none") {
+        section.style.display = "block";    
+    } else {
+        section.style.display = "none"
+    }
+}
