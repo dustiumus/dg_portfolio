07210a184ccf3dfc697e643253d90e9f91263bfe
CaptMoroni <dustiumus@gmail.com>
2025-05-31 17:45:08 -0600

    added new buttons, css and links

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
 function toggleMe() {
     const section = document.getElementById("aboutMe");
-    if (section.style.display === "none") {
-        section.style.display = "block";    
-    } else {
-        section.style.display = "none"
+    section.classList.toggle("show")
+
+
+    // if (section.style.display === "none" || section.style.display === "" ) {
+    //     section.style.display = "block";    
+    // } else {
+    //     section.style.display = "none"
     }
-}
+
