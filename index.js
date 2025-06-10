21c9d7b382a8498eee58780c2a8d559e09966b84
CaptMoroni <dustiumus@gmail.com>
2025-06-09 20:58:12 -0600

    added more styling, final functionality

--- index.js
+++ index.js
@@ -13,3 +13,39 @@ function toggleForm() {
     form.style.display = (form.style.display === "none") ? "block" : "none";
 }
 
+function toggleProject1() {
+    const about = document.getElementById("project1");
+    if (about.style.display === "none" || !about.style.display) {
+        about.style.display = "block";    
+    } else {
+        about.style.display = "none"
+    }
+}
+
+function toggleProject2() {
+    const about = document.getElementById("project2");
+    if (about.style.display === "none" || !about.style.display) {
+        about.style.display = "block";    
+    } else {
+        about.style.display = "none"
+    }
+}
+
+function toggleProject3() {
+    const about = document.getElementById("project3");
+    if (about.style.display === "none" || !about.style.display) {
+        about.style.display = "block";    
+    } else {
+        about.style.display = "none"
+    }
+}
+
+function togglePlans() {
+    const about = document.getElementById("plans");
+    if (about.style.display === "none" || !about.style.display) {
+        about.style.display = "block";    
+    } else {
+        about.style.display = "none"
+    }
+}
+
