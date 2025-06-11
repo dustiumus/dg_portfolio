0d3b8efeec6cb3e73b96ff81235f66fba33745c7
CaptMoroni <dustiumus@gmail.com>
2025-06-11 11:38:20 -0600

    fixed contact button

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ function toggleMe() {
     }
 }
 
+// function toggleForm() {
+//     const form = document.getElementById("contactForm");
+//     form.style.display = (form.style.display === "none") ? "block" : "none";
+// }
+
 function toggleForm() {
-    const form = document.getElementById("contactForm");
-    form.style.display = (form.style.display === "none") ? "block" : "none";
+    const about = document.getElementById("contactForm");
+    if (about.style.display === "none" || !about.style.display) {
+        about.style.display = "block";    
+    } else {
+        about.style.display = "none"
+    }
 }
+
 
 function toggleProject1() {
     const about = document.getElementById("project1");
